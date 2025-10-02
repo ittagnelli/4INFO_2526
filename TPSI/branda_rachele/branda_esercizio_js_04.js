408686dfd93493704e89e168a13210804bc3f222
Branda-Rachele <rachele.branda@istitutoagnellli.it>
2025-10-02 16:39:42 +0200

    (Branda) Consegna Esercitazione JS 04

--- TPSI/branda_rachele/branda_esercizio_js_04.js
+++ TPSI/branda_rachele/branda_esercizio_js_04.js
@@ -1,23 +1,22 @@
 function modifica_stile(){
-    let h1= document.querySelectorAll("h1");
-    h1[0].style.color="red";
-    h1[1].style.color="red";
-
+    let h1 = document.querySelectorAll("h1");
+    for (let j = 0; j < h1.length; j++) {
+    h1[j].style.color="red";
+    }
 
     let h2=document.querySelectorAll("h2");
     for(let j = 0; j < h2.length; j++) {
     h2[j].style.color="blue";
-    h2[j].style.color="blue";
     h2[j].style.backgroundColor="yellow";
-    h2[j].style.fontSize="40px"
-    h2[j].textContent= h2[j].textContent.toUpperCase();
+    h2[j].style.fontSize="40px";
+    h2[j].textContent = h2[j].textContent.toUpperCase();
     }
    
-    let p= document.querySelectorAll(p);
+    let p = document.querySelectorAll("p");
     p[0].style.color="red";
     p[3].style.color="red";
 
-    p[0].style.backgroundColor="lightgreen";
+    p[1].style.backgroundColor="lightgreen";
     p[4].style.backgroundColor="lightgreen";
     p[7].style.backgroundColor="lightgreen";
 
@@ -27,7 +26,4 @@ function modifica_stile(){
     p[5].textContent= p[5].textContent.toUpperCase();
 
     p[6].style.fontStyle="italic";
-
-
-
 }
