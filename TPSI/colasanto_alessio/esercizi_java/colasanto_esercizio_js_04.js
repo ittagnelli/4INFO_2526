a633bf27dde84d97465c73cdcdbde31aa770a6a7
Colasanto-Alessio <a9977239@gmail.com>
2025-10-02 16:42:23 +0200

    .

--- TPSI/colasanto_alessio/esercizi_java/colasanto_esercizio_js_04.js
+++ TPSI/colasanto_alessio/esercizi_java/colasanto_esercizio_js_04.js
@@ -11,5 +11,25 @@ function modifica_stile(){
         secondo_livello[i].innerText=secondo_livello[i].innerText.toUpperCase();
     }
 
+    let p=document.getElementsByTagName("p");
+    for(let i=0;i<p.length;i++){
+        p[i].style.color="red"
+        if (i==0 || i==3){
+        p[i].style.color="red"   
+        }else if (i==1 || i==4 || i==7){
+            p[i].style.backgroundColor="lightgreen"
+
+        }else if(i==2){
+            p[i].innerText=p.innerText.toUpperCase();
+            p[i].style.backgroundColor="lightblue"
+
+
+        }else if(i==5){
+            p[i].innerText=p.innerText.toUpperCase();
+        }else if(i==6){
+            p[i].style.fontFamily="italic"
+        }
+
+    }
 
 }
