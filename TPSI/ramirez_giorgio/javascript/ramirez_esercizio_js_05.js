8f782799beda893cc3a12b433512e9a99983c730
Giorgio Ramirez <giorgio.ramirez@outlook.it>
2025-10-09 16:24:19 +0200

    es05_ramirez

--- TPSI/ramirez_giorgio/javascript/ramirez_esercizio_js_05.js
+++ TPSI/ramirez_giorgio/javascript/ramirez_esercizio_js_05.js
@@ -1,13 +1,29 @@
-alert("CIAO");
-function aggiungiVoto(){
-    let nome = document.getElementById("nome").value ;
-    let media = document.getElementById("media").value ;
+function aggiungiVoto() {
+    let nome = document.getElementById("nome").value;
+    let media = document.getElementById("media").value;
     let tabella = document.getElementById("tabella");
+
     let linea = document.createElement('tr');
     let colnome = document.createElement('td');
     let colmedia = document.createElement('td');
+
     colnome.innerText = nome;
-    colmedia.innerHTML = media; 
+    colmedia.innerText = media; 
+
     linea.append(colnome);
     linea.append(colmedia);
+    tabella.append(linea);
+}
+
+function coloraRighe() {
+    let righe = document.querySelectorAll("#tabella tr");
+
+    for (let i = 1; i < righe.length; i++) {
+        if (i % 2 === 0) {
+            righe[i].style.backgroundColor = "blue";
+        } 
+        else {
+            righe[i].style.backgroundColor = "red";
+        }
+    }
 }
