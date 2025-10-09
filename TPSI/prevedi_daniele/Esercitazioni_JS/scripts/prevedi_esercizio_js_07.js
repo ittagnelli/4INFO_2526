bcf44cea412cce351543b31099855daf67f49ef7
Prevedi-Daniele <danieleprevedi@gmail.com>
2025-10-09 16:45:47 +0200

    .

--- TPSI/prevedi_daniele/Esercitazioni_JS/scripts/prevedi_esercizio_js_07.js
+++ TPSI/prevedi_daniele/Esercitazioni_JS/scripts/prevedi_esercizio_js_07.js
@@ -1,1 +1,30 @@
-document.addEventListener("DOMContentLoaded", init, false)
+document.addEventListener("DOMContentLoaded", init)
+
+function init(){
+    let buttonCerca = document.getElementById("cerca")
+    let buttonSalva = document.getElementById("salva")
+    let buttonAggiorna = document.getElementById("aggiorna")
+    buttonCerca.addEventListener("click", cercaHandler)
+    buttonSalva.addEventListener("click", salvaHandler)
+    buttonAggiorna.addEventListener("click", aggiornaHandler)
+}
+
+function cercaHandler(){
+    console.log("Cerca handler")
+    console.log(getValue("cognome"))
+    console.log(getValue("nome"))
+    console.log(getValue("indirizzo"))
+    console.log(getValue("telefono"))
+}
+
+function salvaHandler(){
+    console.log("Salva handler")
+}
+
+function aggiornaHandler(){
+    console.log("Aggiorna handler")
+}
+
+function getValue(id){
+    return document.getElementById(id).value
+}
