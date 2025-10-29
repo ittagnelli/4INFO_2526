fd4db6c585c0ebb5c804ee2ffbb57a00be6540fc
Lorenzo Ruscica <152178455+Lorenzo-Ruscica@users.noreply.github.com>
2025-10-29 19:11:15 +0100

    Correzione esercizio paghe

--- TPSI/ruscica_lorenzo/JavaScript/ruscica_esercizio_js_paghe.js
+++ TPSI/ruscica_lorenzo/JavaScript/ruscica_esercizio_js_paghe.js
@@ -6,49 +6,77 @@ function salva() {
     let importo = document.getElementById("importo");
     let dipartimento = document.getElementById("dipartimento");
     let tabella = document.getElementById("paghe");
-    console.log(cognome)
-    console.log(nome)
-    console.log(mese)
-    console.log(importo)
-    console.log(dipartimento)
 
     if (cognome.value !== "" && nome.value !== "" && mese.value !== "" && importo.value !== "" && dipartimento.value !== "") {
-        console.log("procedi");
         const tr = document.createElement("tr");
-        const tdcognome = document.createElement("td");
-        const tdnome = document.createElement("td");
-        const tdmese = document.createElement("td");    
-        const tdimporto = document.createElement("td");
-        const tddipartimento = document.createElement("td");
-
-        tdcognome.innerText = cognome.value;
-        tdnome.innerText = nome.value;
-        tdmese.innerText = mese.value;
-        tdimporto.innerText = importo.value;
-        tddipartimento.innerText = dipartimento.value;
-
-        tr.append(tddipartimento);
-        tr.append(tdcognome);
-        tr.append(tdnome);
-        tr.append(tdmese);
-        tr.append(tdimporto);
-
+        tr.innerHTML = `
+            <td>${dipartimento.value}</td>
+            <td>${cognome.value}</td>
+            <td>${nome.value}</td>
+            <td>${mese.value}</td>
+            <td class="importo">${parseFloat(importo.value)}</td>
+        `;
         tabella.append(tr);
 
-        // cognome.value = "";
-        // nome.value = "";
-        // mese.value = "";
-        // importo.value = "";
-        // dipartimento.value = "";
-        const righe = tabella.getElementsByTagName("tr").length;
-        const righeiniziali = 2;
-        const righenuove = righe - righeiniziali;   
-        console.log("Righe aggiunte", righenuove)
+        cognome.value = "";
+        nome.value = "";
+        mese.value = "";
+        importo.value = "";
+        dipartimento.value = "";
     } else {
-        alert("Inserisci tutti i campi");
-    }   
+        alert("Inserisci tutti i campi!");
+    }
 }
 
 function totale() {
+    let tabella = document.getElementById("paghe");
+    let importi = tabella.getElementsByClassName("importo");
+    if (importi.length < 2) {
+        alert("Servono almeno 2 paghe per calcolare il totale!");
+        return;
+    }
+    let somma = 0;
+    for (let i = 0; i < importi.length; i++) {
+        somma += parseFloat(importi[i].innerText);
+    }
 
+    let rigaTotale = document.getElementById("rigaTotale");
+    if (!rigaTotale) {
+        rigaTotale = document.createElement("tr");
+        rigaTotale.id = "rigaTotale";
+        rigaTotale.innerHTML = `
+            <td colspan="4">TOTALE</td>
+            <td class="importo">${somma} €</td>
+        `;
+        tabella.append(rigaTotale);
+    } else {
+        rigaTotale.querySelector(".importo").innerText = somma + " €";
+    }
+}
+
+function media() {
+    let tabella = document.getElementById("paghe");
+    let importi = tabella.getElementsByClassName("importo");
+    if (importi.length < 2) {
+        alert("Servono almeno 2 paghe per calcolare la media!");
+        return;
+    }
+    let somma = 0;
+    for (let i = 0; i < importi.length; i++) {
+        somma += parseFloat(importi[i].innerText);
+    }
+    let media = somma / importi.length;
+
+    let rigaMedia = document.getElementById("rigaMedia");
+    if (!rigaMedia) {
+        rigaMedia = document.createElement("tr");
+        rigaMedia.id = "rigaMedia";
+        rigaMedia.innerHTML = `
+            <td colspan="4">MEDIA</td>
+            <td class="importo">${media.toFixed(2)} €</td>
+        `;
+        tabella.append(rigaMedia);
+    } else {
+        rigaMedia.querySelector(".importo").innerText = media.toFixed(2) + " €";
+    }
 }
